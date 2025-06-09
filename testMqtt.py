6f850cb94cd1fabc6614f675857bf8bc3c8e6433
Sonny Smith <bsmith@primebiz.com>
2025-06-09 14:28:37 -0700

    Add files via upload

---
+++ testMqtt.py
@@ -0,0 +1,33 @@
+from machine import Pin, I2C
+import time 
+import network
+from umqtt.simple import MQTTClient
+import WiFi
+from getTime import formatted_time
+
+
+
+mqtt_server = 'broker.emqx.io'
+client_id = 'bigles4567'
+topic_pub = b'primebiz'
+topic_msg = b'testing from pico w @ ' + formatted_time
+
+
+def mqtt_connect():
+    client = MQTTClient(client_id, mqtt_server, keepalive=3600)
+    client.connect()
+    print('Connected to %s MQTT Broker'%(mqtt_server))
+    return client
+
+def reconnect():
+    print('Failed to connect to the MQTT Broker. Reconnecting...')
+    time.sleep(.5)
+    machine.reset()
+
+try:
+    client = mqtt_connect()
+except OSError as e:
+    reconnect()
+
+client.publish(topic_pub, topic_msg)
+    
